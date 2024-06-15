cb5a673be61ca9686a6c6af29ab2dade46d549ad
shreya <shedgeshreyy03@gmail.com>
2024-06-15 17:44:21 +0530

    bootstrap

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -1,12 +1,18 @@
-import { Button, Button2 } from './Button'
+// import { Button, Button2 } from './Button'
 import './App.css'
 
 const App = () => {
   return <div>
-    <h1>Hello Bhai</h1>
-    <Button></Button>
-    <Button2></Button2>
+  <button type="button" className="btn btn-primary">Primary</button>
+<button type="button" className="btn btn-secondary">Secondary</button>
+<button type="button" className="btn btn-success">Success</button>
+<button type="button" className="btn btn-danger">Danger</button>
+<button type="button" className="btn btn-warning">Warning</button>
+<button type="button" className="btn btn-info">Info</button>
+<button type="button" className="btn btn-light">Light</button>
+<button type="button" className="btn btn-dark">Dark</button>
 
+<button type="button" className="btn btn-link">Link</button>
   </div>
   
 }
