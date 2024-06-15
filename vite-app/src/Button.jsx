279f04fc74f7516e768e7e21c739ae0eb577e6e5
shreya <shedgeshreyy03@gmail.com>
2024-06-15 17:29:55 +0530

    components

---
+++ vite-app/src/Button.jsx
@@ -0,0 +1,13 @@
+//single export
+// const Button = () => {
+//     return <button>Subscribe</button>
+// }
+// export default Button
+
+//multiple exports 
+export function Button() {
+    return <button>Subscribe</button>
+}
+export function Button2() {
+    return <button>Like the video</button>
+}
