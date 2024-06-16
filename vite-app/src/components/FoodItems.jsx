07050c61348a2d37e7fe1f3be0bd3374b573dd00
shreya <shedgeshreyy03@gmail.com>
2024-06-17 02:47:45 +0530

    onChangeBuyButton

--- vite-app/src/components/FoodItems.jsx
+++ vite-app/src/components/FoodItems.jsx
@@ -1,18 +1,24 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 export default function FoodItems({ items }) {
-  const buttonClicked = (item) => {
+
+  let [boughtItems, setBoughtItems] = useState([])
+
+  const buttonClicked = (event,item) => {
     console.log(`${item} bought`);
+    let newItems = [...boughtItems, item]
+    setBoughtItems(newItems)
   };
   return (
     <React.Fragment>
       <ul className="list-group">
         {items.map((item) => (
           <Item
+            bought={boughtItems.includes(item)}
             key={item}
-            buttonClicked={() => buttonClicked(item)}
+            buttonClicked={(event) => buttonClicked(event,item)}
             foodItem={item}
           ></Item>
         ))}
