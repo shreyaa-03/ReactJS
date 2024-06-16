4634216f2130018f6dba45ee5f0c98969a4dd48f
shreya <shedgeshreyy03@gmail.com>
2024-06-16 16:18:56 +0530

    css modules

--- vite-app/src/components/Item.jsx
+++ vite-app/src/components/Item.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
+import style from '../css/Item.module.css'
 export default function Item( {foodItem}) {
     
   return (
     <div>
-       <li className="list-group-item">{foodItem}</li>
+      <li className={`${style['kg-item']} list-group-item `}>{foodItem}</li>
     </div>
   )
 }
