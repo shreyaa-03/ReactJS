32d681ff3550a72963db785ad68e5b0f3fec6fe4
shreya <shedgeshreyy03@gmail.com>
2024-06-16 18:03:22 +0530

    calci-ui-done

--- calculator/src/components/ButtonsContainer.jsx
+++ calculator/src/components/ButtonsContainer.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/jsx-key */
 import style from '../css/ButtonsContainer.module.css'
 
 export default function ButtonsContainer() {
+
+    const buttonsNames = ['C', '1', '2', '+', '3', '4','-','5' , '6', '*', '7', '8', '/', '=', '9', '0', '.']
+
   return (
-          <div className={style["buttons-container"]}>
-          <button >C</button>
+      <div className={style["buttons-container"]}>
+          {buttonsNames.map(buttons => (<button >{buttons}</button>))}
+          
         </div>
    
   )
