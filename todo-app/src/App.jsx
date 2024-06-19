c6a8304304b52ab61d0148dd4e4045012e721e54
shreya <shedgeshreyy03@gmail.com>
2024-06-19 21:24:23 +0530

    functionalUpdate

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -9,9 +9,13 @@ function App() {
 
   const onClicked = (newItem, newDate) => {
     if (newItem && newDate) {
-      const newItems = [...todoItems, { name: newItem, due: newDate }];
-      setTodoItems(newItems);
-      console.log(event.target.value);
+      // SPREAD OPERATOR UPDATE
+      //Drawback -> Since setTodoItems is asynchronousa and is multiple updates are queued, there's a risk that the state might not be the latest version when the update occurs.
+      // const newItems = [...todoItems, { name: newItem, due: newDate }];
+      // setTodoItems(newItems);
+
+      //SETTING VALUE BY PASSING FUNCTION -> this is better
+      setTodoItems((currValue)=> [...currValue,{name: newItem, due: newDate}])
     } else {
       alert("Enter both: Name and date");
     }
