d8a3db05bf68244b764f2b23e8c72f7bea1e105b
shreya <shedgeshreyy03@gmail.com>
2024-06-21 00:27:15 +0530

    social-media_UI

--- social-media/src/App.jsx
+++ social-media/src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import "./App.css";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
 
 export default function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
+
+  const setActiveTab = () => {
+    
+  }
+
   return (
-    <div>
-      
+    <div className="app-container">
+      <Sidebar selectedTab={selectedTab}></Sidebar>
+      <div className="content">
+        <Header></Header>
+        {selectedTab === "Home" ? (
+          <PostList></PostList>
+        ) : (
+          <CreatePost></CreatePost>
+        )}
+
+        <Footer></Footer>
+      </div>
     </div>
-  )
+  );
 }
---
+++ social-media/src/components/PostList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Post from './Post'
+
+export default function PostList() {
+    return (
+        <>
+    <Post></Post>
+    <Post></Post>
+      <Post></Post>
+      </>
+  )
+}
