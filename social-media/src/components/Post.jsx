f08a9128583bdbb77e103e51744b3406af78a189
shreya <shedgeshreyy03@gmail.com>
2024-06-21 16:58:20 +0530

    posttt

--- social-media/src/components/Post.jsx
+++ social-media/src/components/Post.jsx
@@ -1,13 +1,32 @@
+/* eslint-disable react/prop-types */
+import { useContext } from "react";
+import { MdDelete } from "react-icons/md";
+import  { PostListContext } from "../store/post-list-store";
 
-export default function Post({postList}) {
+export default function Post({ postList }) {
+
+  const { deletePost } = useContext(PostListContext)
+
   return (
-    <div className="card" style={{width: "18rem"}}>
-    <div className="card-body">
-        <h5 className="card-title">{ postList.title}</h5>
-      <p className="card-text">{ postList.body}</p>
-      <a href="#" className="btn btn-primary">Go somewhere</a>
+    <div className="card post-card">
+      <div className="card-body">
+        <h5 className="card-title">
+          {postList.title}
+          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+          onClick={() => deletePost(postList.id)} >
+          <MdDelete />
+          </span>
+        </h5>
+        <p className="card-text">{postList.body}</p>
+        {postList.tags.map((tag) => (
+          <span className="badge rounded-pill text-bg-primary hashtag" key={tag}>
+            {tag}
+          </span>
+        ))}
+        <div className="alert alert-success reactions" role="alert">
+          This post has been reacted by {postList.reactions} people.
+        </div>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
-
--- social-media/src/store/post-list-store.jsx
+++ social-media/src/store/post-list-store.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from "react";
+import { createContext, useReducer } from "react";
 
 export const PostListContext = createContext({
   postList: [],
@@ -10,10 +10,15 @@ const postListReducer = (currentValue, action) => {};
 
 const PostListProvider = ({ children }) => {
   //useReducer(reducerFunction, initialValue)
-  const [postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST);
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
 
   const addPost = () => {};
-  const deletePost = () => {};
+  const deletePost = (postId) => {
+    console.log(`Delete post id: ${postId}`)
+  };
 
   return (
     <PostListContext.Provider value={{ postList, addPost, deletePost }}>
@@ -23,22 +28,22 @@ const PostListProvider = ({ children }) => {
 };
 
 const DEFAULT_POST_LIST = [
-    {
-    id: '1',
-    title: 'Going to Bangalore',
-    body: 'Hello guys! Going for my vacs. Hope to enjoy',
-    reactions: 2,
-    userId: 'user-1',
-    tags:['vacs','enjoy','peace']
-},
-    {
-    id: '2',
-    title: 'Pass hogaya bhai',
-    body: 'Bhai masti masti mein btech khatam hogaya,pata nhai chala',
-    reactions: 2,
-    userId: 'user-2',
-    tags:['pass','result']
-}
-]
+  {
+    id: "1",
+    title: "Going to Bangalore",
+    body: "Hello guys! Going for my vacs. Hope to enjoy",
+    reactions: 8,
+    userId: "user-1",
+    tags: ["vacs", "enjoy", "peace"],
+  },
+  {
+    id: "2",
+    title: "Pass hogaya bhai",
+    body: "Bhai masti masti mein btech khatam hogaya,pata nhai chala",
+    reactions: 18,
+    userId: "user-2",
+    tags: ["pass", "result"],
+  },
+];
 
 export default PostListProvider;
