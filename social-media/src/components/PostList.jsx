76fbb9fa316a251123efde176cf991bd2d27dcfc
shreya <shedgeshreyy03@gmail.com>
2024-06-21 18:33:44 +0530

    okk

--- social-media/src/components/PostList.jsx
+++ social-media/src/components/PostList.jsx
@@ -1,13 +1,14 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import Post from "./Post";
 import { PostListContext } from "../store/post-list-store";
 
 export default function PostList() {
-
-    const { postList } = useContext(PostListContext)
+  const { postList } = useContext(PostListContext);
   return (
     <>
-      {postList.map((post) => (<Post key={post.id} postList={post}></Post>))}
+      {postList.map((post) => (
+        <Post key={post.id} postList={post}></Post>
+      ))}
     </>
   );
 }
--- social-media/src/store/post-list-store.jsx
+++ social-media/src/store/post-list-store.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { createContext, useReducer } from "react";
 
 export const PostListContext = createContext({
